af161f485a9ce29111f9ba5728d9269dc9af0547
Naveen Kumar R <159445406+naveenkumar-ramesh2004@users.noreply.github.com>
2025-10-05 20:27:46 +0530

    2511. Maximum Enemy Forts That Can Be Captured.java

---
+++ Day 49(Array)/2511. Maximum Enemy Forts That Can Be Captured.java
@@ -0,0 +1,24 @@
+// 2511. Maximum Enemy Forts That Can Be Captured
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0){return 0;}
+        Set<Integer> arr= new HashSet<>();
+        for(int val:nums){
+            arr.add(val);
+        }
+        int count=1,max=1;
+        for(int val:arr){
+            if(!arr.contains(val-1)){
+                count=1;
+                while(arr.contains(++val)){
+                    count++; 
+                }
+                max=Math.max(max,count);
+            }
+            
+        }
+        return max;
+    }
+}
