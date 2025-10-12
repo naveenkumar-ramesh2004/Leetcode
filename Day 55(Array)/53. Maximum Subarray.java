cdfb0ff8429af6e1653f3d8eadaf5a7b088087b2
Naveen Kumar R <159445406+naveenkumar-ramesh2004@users.noreply.github.com>
2025-10-12 23:40:19 +0530

    53. Maximum Subarray.java

---
+++ Day 55(Array)/53. Maximum Subarray.java
@@ -0,0 +1,57 @@
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/description/
+
+
+/*class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){   //O(n^2)
+            int sum=0;
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+                max=Math.max(max,sum);
+                }
+            }
+        return max;
+    }
+}*/
+
+
+/*
+Prefix Sum + Greedy Reset Pattern
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+           sum=Math.max(nums[i],sum+nums[i]);
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+}
+
+
+/*
+💡 Dry Run Example
+
+For: nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+i	nums[i]	sum (current)	max
+0	-2	-2	-2
+1	1	max(1, -2+1)=1	1
+2	-3	max(-3, 1-3)=-2	1
+3	4	max(4, -2+4)=4	4
+4	-1	max(-1, 4-1)=3	4
+5	2	max(2, 3+2)=5	5
+6	1	max(1, 5+1)=6	6
+7	-5	max(-5, 6-5)=1	6
+8	4	max(4, 1+4)=5	6
+
+✅ Final answer: 6
+
+*/
