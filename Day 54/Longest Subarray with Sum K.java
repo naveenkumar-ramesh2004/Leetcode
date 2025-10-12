683dc9d3a46a477a08e6d6506104b110ea09c9b1
Naveen Kumar R <159445406+naveenkumar-ramesh2004@users.noreply.github.com>
2025-10-12 21:47:08 +0530

    Update Longest Subarray with Sum K.java

--- Day 54/Longest Subarray with Sum K.java
+++ Day 54/Longest Subarray with Sum K.java
@@ -2,30 +2,7 @@
 // https://www.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
 
 
-// // User function Template for Java
-
-// class Solution {
-//     public int longestSubarray(int[] arr, int k) {
-//         // code here
-//         int sum=0,count=1;
-//         int max=0;
-//         for(int i=0;i<arr.length;i++){
-//              sum=0;
-//             count=0;
-//             for(int j=i;j<arr.length;j++){
-//                     sum+=arr[j];
-//                     count++;
-//                     if(sum==k){
-//                   max=Math.max(max,count);
-//             }
-            
-            
-//             }
-//         }
-//         return max;
-//     }
-// }
-
+//use hashmap
 class Solution {
     public int longestSubarray(int[] arr, int k) {
         int sum=0,lengthsubarray=0;
@@ -46,3 +23,61 @@ class Solution {
           
     }
 }
+
+/*
+import java.util.*;
+public class sumksubarray{
+    public static void main(String[] args) {
+        // int arr[]={1,1,2,-1,1,1,1};
+        int arr[]={10, 5, 2, 7, 1, 9};
+        int k=15;
+        System.out.println(brute(arr,k));
+        System.out.print(optimal(arr,k));
+    }
+    static int brute(int[] arr,int k){
+        int max=0;
+        // for(int i=0;i<arr.length;i++){    //O(n^3)
+        //     for(int j=i;j<arr.length;j++){
+        //         int sum=0;
+        //         int count=0;
+        //         for(int t=i;t<=j;t++){
+        //             sum+=arr[t];
+        //             count++;
+        //     }
+        //     if(sum==k){
+        //         max=Math.max(max,count);
+        //         }
+        //     }
+        // }
+        for(int i=0;i<arr.length;i++){   //O(n^2)
+            int sum=0;
+            int count=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                count++;
+                if(sum==k){
+                max=Math.max(max,count);
+                }
+            }
+        }
+        return max;
+    }
+    static int optimal(int[] arr,int k){   //O(n) only the array have +ve only
+        int i=0,j=0;
+        int sum=0,lengthOfMaxSubAraay=0;
+        while(i<=j && j<arr.length){
+            sum+=arr[j];
+            if(sum == k){
+                lengthOfMaxSubAraay=Math.max(lengthOfMaxSubAraay,j-i+1);
+            }
+            while(sum>k){
+                sum-=arr[i++];
+                System.out.println("sum: "+sum);
+            }
+            j++;
+            
+        }
+        return lengthOfMaxSubAraay;
+    }
+}
+*/
